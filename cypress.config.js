1c9250cbc1df90ba8d78fa8352f6d3daf5009482
Bruno Tineli <bruno.tineli@gmail.com>
2025-05-07 00:07:48 -0300

    Configure Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // Cypress Cloud
+  projectId: "on7ry1",
+  // Default configs
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
